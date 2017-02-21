57512e79f5edb7810df738d95bd720bbf05d1645
Filip Zivkovic <fzivkovic@apple.com>
2017-02-20 17:21:50 -0800

    Small changes

--- execute.py
+++ execute.py
@@ -232,8 +232,17 @@ def decode():
       # Get token-ids for the input sentence.
       token_ids = data_utils.sentence_to_token_ids(tf.compat.as_bytes(sentence), enc_vocab)
       # Which bucket does it belong to?
-      bucket_id = min([b for b in xrange(len(_buckets))
-                       if _buckets[b][0] > len(token_ids)])
+      print('Length token ids:')
+      print(len(token_ids))
+      potentialBuckets = [b for b in xrange(len(_buckets))
+                       if _buckets[b][0] > len(token_ids)]
+      if not potentialBuckets:
+        print("Too long of input. Continuing.")
+        print("> ", end="")
+        sys.stdout.flush()
+        sentence = sys.stdin.readline()
+        continue
+      bucket_id = min(potentialBuckets)
       # Get a 1-element batch to feed the sentence to the model.
       encoder_inputs, decoder_inputs, target_weights = model.get_batch(
           {bucket_id: [(token_ids, [])]}, bucket_id)
