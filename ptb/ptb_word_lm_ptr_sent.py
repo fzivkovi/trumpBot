0418ad6235df764b158ec6947f00959f53b3db9f
Filip Zivkovic <fzivkovic@apple.com>
2017-03-18 20:18:50 -0700

    Fixed a little bug with saver not moving things into the directory.

--- ptb/ptb_word_lm_ptr_sent.py
+++ ptb/ptb_word_lm_ptr_sent.py
@@ -739,7 +739,7 @@ def main(_):
         if FLAGS.save_path and bestRunningValidationPerplexity >= valid_perplexity:
           print("Saving model to %s." % FLAGS.save_path)
           #global_step = "epoch_%s_valid_perp_%s" % (i,valid_perplexity)
-          sv.saver.save(session, FLAGS.save_path, global_step=i)#, max_to_keep=None)
+          sv.saver.save(session, os.path.join(FLAGS.save_path, 'epoch.txt'), global_step=i, max_to_keep=3)
 
         if FLAGS.test:
           test_perplexity = run_epoch(session, mtest,ids_to_words=ids_to_words)
